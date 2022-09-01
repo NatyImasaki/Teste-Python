ab790356c9287f6f3ec2dd6bdc42b41ebe68b093
NatyImasaki <naty.imasaki@gmail.com>
2022-09-01 11:45:18 -0300

    novo arquivo

---
+++ nome_sobrenome.py
@@ -0,0 +1,7 @@
+print("Esse programa exibira seu nome completo. ")
+nome = input("Insira seu nome: ")
+sobrenome = input("Insira seu sobrenome: ")
+
+nome_completo = nome + " " + sobrenome
+
+print(nome_completo)
