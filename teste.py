8e600ef3443846c57acacf2fd120388a78d15028
NatyImasaki <naty.imasaki@gmail.com>
2022-09-01 11:06:08 -0300

    first commit

---
+++ teste.py
@@ -0,0 +1,9 @@
+print("Ola mundo")
+nome = input("Digite seu nome: ")
+print(nome + " e um programador incrivel!")
+
+ano_nascimento = input("Digite seu ano de nascimento: ")
+ano_atual = input("Digite o ano atual: ")
+
+idade = int(ano_atual) - int(ano_nascimento)
+print(idade)
