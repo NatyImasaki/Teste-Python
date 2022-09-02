8dfdd657a0200f46915844cec3b5475caf97974f
NatyImasaki <naty.imasaki@gmail.com>
2022-09-02 17:45:34 -0300

    troca variavel

---
+++ troca_var.py
@@ -0,0 +1,10 @@
+print("Este programa inverte o conteudo de duas variaveis.")
+A = input("Insira o conteudo da váriavel 1: ")
+B = input("Insira o conteudo da variavel B: ")
+print("O valor inicial da variavel A é: {} e o da variavel B é: {}".format(A, B))
+troca = A
+A = B
+B = troca
+
+print("Agora que trocamos, o valor da varivel A é: {} e o da B é: {}".format(A, B))
+
