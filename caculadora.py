20fb3d63e1053822c43d33968431b1f6e879666f
NatyImasaki <naty.imasaki@gmail.com>
2022-09-02 15:51:02 -0300

    add calculadora file

---
+++ caculadora.py
@@ -0,0 +1,14 @@
+valor1 = input("Insira o primeiro valor: ")
+valor2 = input("Insira o segundo valor: ")
+
+soma = float(valor1) + float(valor2)
+print("O total da soma é: {}".format(soma))
+
+subtracao = float(valor1) - float(valor2)
+print("O total da subtração é: {}".format(subtracao))
+
+divisao = float(valor1) / float(valor2)
+print("O total da divisão é: {}".format(divisao))
+
+multiplicacao = float(valor1) * float(valor2)
+print("O total da multiplicação é: {}".format(multiplicacao))
