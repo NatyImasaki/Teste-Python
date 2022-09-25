180118c295a76a8407fd92bae348094cc98cd7ab
NatyImasaki <naty.imasaki@gmail.com>
2022-09-25 17:04:48 -0300

    exercicios while

---
+++ exemplo_while.py
@@ -0,0 +1,6 @@
+resposta = "0"
+
+while (resposta != "42"):
+    resposta = input("Qual a reposta para a vida, o universo e tudo mais?")
+
+print("Resposta correta!")
---
+++ exemplo_while_contador.py
@@ -0,0 +1,6 @@
+i = 0
+
+while (i < 10):
+    print("Mais uma mensagem, com i valendo {}".format(i))
+    i = i + 1
+
