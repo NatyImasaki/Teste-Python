757fbd79441124a84b7d2367d56221b5bb041c55
NatyImasaki <naty.imasaki@gmail.com>
2022-09-26 15:45:04 -0300

    funcoes

--- Funcoes/iniciando_funcao.py
+++ Funcoes/iniciando_funcao.py
@@ -1,5 +1,12 @@
 def calculaVelocidadeMedia(distancia, tempo):
     velocidadeMedia = distancia /tempo
-    print("A velocidade média é de {0:.2f} km/h".format(velocidadeMedia))
+    return velocidadeMedia
 
-calculaVelocidadeMedia(200, 30)
+
+dist = float(input("Insira a distancia percorrida: "))
+tempo = float(input("Insira o tempo da viagem: "))
+
+print("A velocidade média é de {0:.2f} km/h".format(calculaVelocidadeMedia(dist, tempo)))
+
+
+
